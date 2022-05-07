97c6f84bc0d24a8be3ab50642509a69f4bf27ab5
developersam <samrudh.kumar@gojek.com>
2022-05-07 13:42:28 +0530

    WIP

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,3 +1,6 @@
+
+
+
 local log = hs.logger.new('init.lua', 'debug')
 
 -- Use Control+` to reload Hammerspoon config
@@ -5,9 +8,11 @@ hs.hotkey.bind({'ctrl'}, '`', nil, function()
   hs.reload()
 end)
 
+hs.hotkey.bind({"cmd", "ctrl"}, "V", function() hs.eventtap.keyStrokes(hs.pasteboard.getContents()) end)
+
 keyUpDown = function(modifiers, key)
   -- Un-comment & reload config to log each keystroke that we're triggering
-  -- log.d('Sending keystroke:', hs.inspect(modifiers), key)
+  log.d('Sending keystroke:', hs.inspect(modifiers), key)
 
   hs.eventtap.keyStroke(modifiers, key, 0)
 end
@@ -31,11 +36,11 @@ enableHotkeyForWindowsMatchingFilter = function(windowFilter, hotkey)
   end)
 end
 
-require('keyboard.control-escape')
-require('keyboard.delete-words')
-require('keyboard.hyper')
-require('keyboard.markdown')
-require('keyboard.panes')
-require('keyboard.windows')
+-- require('keyboard.control-escape')
+-- require('keyboard.delete-words')
+-- require('keyboard.hyper')
+-- require('keyboard.markdown')
+-- require('keyboard.panes')
+-- require('keyboard.windows')
 
 hs.notify.new({title='Hammerspoon', informativeText='Ready to rock 🤘'}):send()
