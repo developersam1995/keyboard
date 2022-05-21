41b1b0037743016a2e07b4ad82b14ed183b5ec6e
developersam <samrudh.kumar@gojek.com>
2022-05-21 10:39:17 +0530

    Refactor

--- hammerspoon/windows.lua
+++ hammerspoon/windows.lua
@@ -1,6 +1,8 @@
 hs.window.animationDuration = 0
 window = hs.getObjectMetatable("hs.window")
 
+local log = hs.logger.new('test.debugger', 'debug')
+
 -- +-----------------+
 -- |        |        |
 -- |  HERE  |        |
@@ -81,8 +83,8 @@ function window.upLeft(win)
 
   f.x = max.x
   f.y = max.y
-  f.w = max.w/2
-  f.h = max.h/2
+  f.w = max.w / 2
+  f.h = max.h / 2
   win:setFrame(f)
 end
 
@@ -98,8 +100,8 @@ function window.downLeft(win)
 
   f.x = max.x
   f.y = max.y + (max.h / 2)
-  f.w = max.w/2
-  f.h = max.h/2
+  f.w = max.w / 2
+  f.h = max.h / 2
   win:setFrame(f)
 end
 
@@ -115,8 +117,8 @@ function window.downRight(win)
 
   f.x = max.x + (max.w / 2)
   f.y = max.y + (max.h / 2)
-  f.w = max.w/2
-  f.h = max.h/2
+  f.w = max.w / 2
+  f.h = max.h / 2
 
   win:setFrame(f)
 end
@@ -133,8 +135,8 @@ function window.upRight(win)
 
   f.x = max.x + (max.w / 2)
   f.y = max.y
-  f.w = max.w/2
-  f.h = max.h/2
+  f.w = max.w / 2
+  f.h = max.h / 2
   win:setFrame(f)
 end
 
@@ -149,7 +151,7 @@ function window.centerWithFullHeight(win)
   local max = screen:fullFrame()
 
   f.x = max.x + (max.w * 5 / 18)
-  f.w = max.w * 4/9
+  f.w = max.w * 4 / 9
   f.y = max.y
   f.h = max.h
   win:setFrame(f)
@@ -167,7 +169,7 @@ function window.shiftLeft(win)
 
   f.x = max.x
   f.y = max.y
-  f.w = max.w * (5/18)
+  f.w = max.w * (5 / 18)
   f.h = max.h
   win:setFrame(f)
 end
@@ -182,9 +184,9 @@ function window.shiftRight(win)
   local screen = win:screen()
   local max = screen:frame()
 
-  f.x = max.x + (max.w * 13/18)
+  f.x = max.x + (max.w * 13 / 18)
   f.y = max.y
-  f.w = max.w * (5/18)
+  f.w = max.w * (5 / 18)
   f.h = max.h
   win:setFrame(f)
 end
@@ -202,58 +204,20 @@ function window.nextScreen(win)
   end
 end
 
-local function empty(s)
+local function isEmpty(s)
   return s == nil or s == ''
-end
-
-
-local log = hs.logger.new('test.debugger', 'debug')
-
-local function renderMainWindowAt (app, windowLoc) 
-  local win = app:mainWindow()
-  if empty(win) then
-    return false
-  end
-  win[windowLoc](win)
-  return true
-end
-
-local function renderAt (bundleID, windowLoc) 
-  local app = hs.application.open(bundleID)
-  local renderSuccess = renderMainWindowAt(app, windowLoc)
-  if not renderSuccess then
-    hs.timer.doAfter(3, function ()
-      renderMainWindowAt(app, windowLoc)
-    end)
-  end
-end
-
-
-local function renderLayout(stack)
-  if not empty(stack[3]) then
-    renderAt(stack[3], 'shiftRight')
-  end
-
-  if not empty(stack[2]) then
-    renderAt(stack[2], 'shiftLeft')
-  end
-
-  if not empty(stack[1])then
-    renderAt(stack[1], 'centerWithFullHeight')
-  end
 end
 
 local function newStack(currentStack, top)
   local resultingStack = {}
   local done = {}
 
-  resultingStack[1] = top 
+  resultingStack[1] = top
 
   done[top] = true
   local newIdx = 2
 
-  for _, v in pairs(currentStack)
-  do
+  for _, v in pairs(currentStack) do
     if not done[v] and newIdx < 5 then
       resultingStack[newIdx] = v
       newIdx = newIdx + 1
@@ -266,44 +230,69 @@ end
 local function deleteFromStack(currentStack, toDelete)
   local resultingStack = {}
   local idx = 0
-  for _, v in pairs(currentStack)
-  do
+  for _, v in pairs(currentStack) do
     if not v == toDelete then
-      resultingStack[idx] = v 
+      resultingStack[idx] = v
       idx = idx + 1
-    end 
+    end
   end
   return resultingStack
 end
 
+local function renderMainWindowAt(app, windowLoc)
+  local win = app:mainWindow()
+  if isEmpty(win) then
+    return false
+  end
+  win[windowLoc](win)
+  return true
+end
 
+local function renderAt(bundleID, windowLoc)
+  local app = hs.application.open(bundleID)
+  local renderSuccess = renderMainWindowAt(app, windowLoc)
+  if not renderSuccess then
+    hs.timer.doAfter(3, function()
+      renderMainWindowAt(app, windowLoc)
+    end)
+  end
+end
 
-hs.urlevent.bind('windowModify', function (eventName, params)
+local function render(stack)
+  if not isEmpty(stack[3]) then
+    renderAt(stack[3], 'shiftRight')
+  end
+
+  if not isEmpty(stack[2]) then
+    renderAt(stack[2], 'shiftLeft')
+  end
+
+  if not isEmpty(stack[1]) then
+    renderAt(stack[1], 'centerWithFullHeight')
+  end
+end
+
+hs.urlevent.bind('windowModify', function(eventName, params)
   local fw = hs.window.focusedWindow()
-  fw[params.fn](fw) 
+  fw[params.fn](fw)
 end)
 
 local windowStack = {}
 
-hs.urlevent.bind('stackWindow', function (eventName, params)
+hs.urlevent.bind('stackWindow', function(eventName, params)
   windowStack = newStack(windowStack, params.bundleID)
-  renderLayout(windowStack)
+  render(windowStack)
 end)
 
-hs.urlevent.bind('quitApp', function (eventName, params)
+hs.urlevent.bind('quitApp', function(eventName, params)
   local frontMost = hs.application.frontmostApplication()
   windowStack = deleteFromStack(windowStack, frontMost:bundleID())
   frontMost:kill()
 end)
 
-hs.urlevent.bind('quitFocusedWindow', function (eventName, params)
-  windowStack = newStack(windowStack, params.bundleID)
-  renderLayout(windowStack)
-end)
-
 function window.test(win)
   hs.application.launchOrFocusByBundleID('com.google.Chrome')
   local fw = hs.window.focusedWindow()
-  -- fw['shiftLeft'](fw) 
-  log.d('windowsafari',hs.inspect(fw))
+  -- fw['shiftLeft'](fw)
+  log.d('windowsafari', hs.inspect(fw))
 end
